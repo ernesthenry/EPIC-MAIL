8068df72ee0722bd45c4c6e2724842d39c621d87
ernesthenry <henry38ernest@gmail.com>
2019-03-21 16:40:08 -0700

    Add validations to input fields Added navigation from the register to the login page Added a thread to email Added the option to retract messages Removed the edit button [#164795546]

--- UI/javascript/script.js
+++ UI/javascript/script.js
@@ -8,13 +8,10 @@ function login(email, password){
             //redirect to user
         window.location.href = "./user.html";
         }
-
-       else{
-
+        else{
            const errormessage = document.getElementById("error");
            errormessage.innerText = "Invalid login"
        }
-
   }
   
   const loginForm = document.getElementById("login_form");
@@ -24,4 +21,29 @@ function login(email, password){
       const password = document.getElementById("password").value; 
       login(email, password)
     })
-  
+
+function validateRegistration(email, firstname, lastname, password){
+    if(email==="" && password ===""){
+        const errormessage = document.getElementById("error");
+        errormessage.innerText = "Please fill out this field"    
+    }
+
+    if (register_form.firstname.value == '' || register_form.lastname.value == ''){
+        const errormessage = document.getElementById("error");
+        errormessage.innerText = "These are required fields"
+    }
+    else{
+        const message = document.getElementById("msg");
+        errormessage.innerText = "You have successfully created an account"
+    }
+}
+
+const registerForm = document.getElementById("register_form");
+registerForm.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    const email = document.getElementById("email").value;
+    const firstname = document.getElementById("fname").value;
+    const lastname = document.getElementById("lname").value;
+    const password = document.getElementById("password").value; 
+    validateRegistration(email, firstname, lastname, password)
+})

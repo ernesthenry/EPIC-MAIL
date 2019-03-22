0e89e8d460eff7b0b7ea90e6a20cff69e10cf657
ernesthenry <henry38ernest@gmail.com>
2019-03-21 18:29:42 -0700

    Add validations to the password input fields [#164795546]

--- UI/javascript/script.js
+++ UI/javascript/script.js
@@ -1,17 +1,19 @@
+var attempt = 3;
 function login(email, password){
 
     if(email==="admin@gmail.com" && password ==="admin123"){
-
-        window.location.href = "./admin.html";
+        alert('Login successfull')
+        window.location.href = "./admin.html"; // Redirecting to admin page.
     }
-    if (login_form.email.value == 'user@gmail.com' && login_form.password.value == 'user123'){
-            //redirect to user
-        window.location.href = "./user.html";
-        }
-        else{
+    else if (login_form.email.value == 'user@gmail.com' && login_form.password.value == 'user123'){
+        alert('Login successfull')
+        window.location.href = "./user.html";    //redirect to user
+    }
+    else{
            const errormessage = document.getElementById("error");
            errormessage.innerText = "Invalid login"
        }
+       
   }
   
   const loginForm = document.getElementById("login_form");
